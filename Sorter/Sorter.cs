4894f9afe91fd82826faba528ccd8ba7d70e2c2b
unknown <C:\Program Files (x86)\The Bat!\MAIL>
2021-11-09 21:21:39 +0300

    Commit

---
+++ Sorter/Sorter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorter
+{
+    class Sorter
+    {
+
+        public void Sort(string filePathIn)
+        {
+          
+        }
+        public void Comparer(string filePathIn)
+        {
+            StreamReader streamReader = new StreamReader(filePathIn);
+            while (streamReader.Peek() != -1)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+               
+
+
+                }
+                streamReader.Read();
+            
+
+            
+        }
+
+    }
+}
